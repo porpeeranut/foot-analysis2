a642ca6474bf2fa6e024f4b4fd59f3233177260a
porpeeranut <porpeeranut@github.com>
2015-04-02 02:20:10 +0700

    get bitmap from video

--- app/src/main/java/com/porpeeranut/footanalysis2/MainActivity.java
+++ app/src/main/java/com/porpeeranut/footanalysis2/MainActivity.java
@@ -1,39 +1,105 @@
 package com.porpeeranut.footanalysis2;
 
+import android.app.Activity;
+import android.app.ProgressDialog;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.media.MediaMetadataRetriever;
+import android.net.Uri;
+import android.os.Environment;
+import android.provider.MediaStore;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+import java.io.File;
+import java.util.ArrayList;
 
 
 public class MainActivity extends ActionBarActivity {
+
+    Activity act;
+    ImageView imageView;
+    Bitmap siden;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        act = this;
+
+        imageView = (ImageView) findViewById(R.id.imageView);
+        Button btnRec = (Button) findViewById(R.id.btnRec);
+        btnRec.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                imageView.setImageBitmap(siden);
+
+                /*Intent intent = new Intent(MediaStore.ACTION_VIDEO_CAPTURE);
+                Uri fileUri = getOutputMediaFileUri();
+                intent.putExtra(MediaStore.EXTRA_OUTPUT, fileUri);  // set the image file name
+                intent.putExtra(MediaStore.EXTRA_VIDEO_QUALITY, 1); // set the video image quality (high at 1)
+
+                if (intent.resolveActivity(getPackageManager()) != null) {
+                    startActivityForResult(intent, 1);
+                }*/
+            }
+        });
+
+        siden = BitmapFactory.decodeResource(getResources(), R.drawable.siden);
+        Bitmap sideup = BitmapFactory.decodeResource(getResources(), R.drawable.sideup);
+        imageView.setImageBitmap(siden);
     }
 
-
     @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == 1 && resultCode == RESULT_OK) {
+            //Uri videoUri = data.getData();
+            //Toast.makeText(this, "Video saved to:\n" + data.getData(), Toast.LENGTH_LONG).show();
+            //mVideoView.setVideoURI(videoUri);
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
+            //final ProgressDialog dialog = ProgressDialog.show(this, "Loading", "Please wait...", true);
+            runOnUiThread(new Runnable() {
+                public void run() {
+                    File mediaStorageDir = new File(Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DCIM), "Camera");
+                    String mp4path = mediaStorageDir.getPath() + File.separator + "VID_foot.mp4";
 
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
+
+                    final MediaMetadataRetriever mediaMetadataRetriever = new MediaMetadataRetriever();
+                    mediaMetadataRetriever.setDataSource(mp4path);
+                    ArrayList<Bitmap> bmFrame = new ArrayList<Bitmap>();
+                    long durationMs = Long.parseLong(mediaMetadataRetriever.extractMetadata(MediaMetadataRetriever.METADATA_KEY_DURATION));
+                    for (int i = 0; i < durationMs; i += 500)
+                        bmFrame.add(mediaMetadataRetriever.getFrameAtTime(i * 1000));
+                    imageView.post(new Runnable() {
+                        public void run() {
+                            imageView.setImageBitmap(mediaMetadataRetriever.getFrameAtTime(0 * 1000));
+                        }
+                    });
+                    //dialog.dismiss();
+                    Toast.makeText(act, "" + bmFrame.size(), Toast.LENGTH_LONG).show();
+                }
+            });
         }
+    }
 
-        return super.onOptionsItemSelected(item);
+    private static Uri getOutputMediaFileUri(){
+        File mediaStorageDir = new File(Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DCIM), "Camera");
+
+        if (! mediaStorageDir.exists()){
+            if (! mediaStorageDir.mkdirs()){
+                Log.d("Camera", "failed to create directory");
+                return null;
+            }
+        }
+        File mediaFile = new File(mediaStorageDir.getPath() + File.separator + "VID_foot.mp4");
+        return Uri.fromFile(mediaFile);
     }
 }
